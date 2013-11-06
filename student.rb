b1c719ca9c8004ec878b26215b380dabe26cec43
Mike Capper <mikebcapp@gmail.com>
2013-11-06 22:55:12 +1300

    Juniors method added

--- student.rb
+++ student.rb
@@ -11,8 +11,12 @@ class Student
 		grade == 12
 	end
 
+	def junior?
+		grade < 12
+	end
+
 	def to_s
-		"#{last_name}, #{first_name}"
+		" * #{last_name}, #{first_name}"
 	end
 end
 
@@ -20,11 +24,23 @@ def seniors(students)
 	students.select { |student| student.senior? }
 end
 
+def juniors(students)
+	students.select { |student| student.junior? }
+end
+
 fred = Student.new("Fred", "James", 12)
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
+puts "Seniors"
+
 seniors(all_students).each do |student|
-	puts student
+		puts student
+end
+
+puts "Juniors"
+
+juniors(all_students).each do |student|
+		puts student
 end
